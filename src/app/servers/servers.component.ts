3b5595ce4c979ae6b38b8becca9c2813367ea201
sonance207 <sonance207@gmail.com>
2017-08-16 18:06:30 -0500

    event binding dynamic

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -23,6 +24,12 @@ export class ServersComponent implements OnInit {
    this.serverCreationStatus = 'Server was created ';
 
   }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+
+  }
+
 
 }
 
