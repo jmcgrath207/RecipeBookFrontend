19a4b44b8e277ab27c72d25013e25eeb68f7b1b7
sonance207 <sonance207@gmail.com>
2017-09-24 22:45:59 -0500

    transform http data to prevent errors

--- src/app/SharedModel/data-storage.service.ts
+++ src/app/SharedModel/data-storage.service.ts
@@ -3,6 +3,7 @@ import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {RecipeService} from '../services/recipe.service';
 import {RecipeModel} from '../recipes/recipe.model';
+import 'rxjs';
 
 @Injectable()
 export class DataStorageService {
@@ -16,9 +17,21 @@ export class DataStorageService {
     }
 
     getRecipes() {
-      this.http.get('https://angular4recipe.firebaseio.com/recipes.json').subscribe(
-        (response: Response) => {
-        const recipes: RecipeModel[] = response.json();
+      this.http.get('https://angular4recipe.firebaseio.com/recipes.json')
+        .map(
+          (response: Response) => {
+            const recipes: RecipeModel[] = response.json();
+            for ( let i = 0; i < recipes.length; i++) {
+              if (!recipes[i]['ingredients']) {
+                console.log(recipes[i]);
+                recipes[i]['ingredients'] = [];
+              }
+            }
+            return recipes;
+          }
+        )
+        .subscribe(
+        (recipes: RecipeModel[]) => {
         this.recipeService.setRecipes(recipes);
         }
       );
