d5327b4677a13229717b86378c6795ab9e6067fd
sonance207 <sonance207@gmail.com>
2017-09-26 23:57:34 -0500

    signing up users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import {RecipeService} from './services/recipe.service';
 import {DataStorageService} from './SharedModel/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
+import {AuthService} from './auth/auth.service';
 
 
 
@@ -51,7 +52,7 @@ import { SigninComponent } from './auth/signin/signin.component';
     ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
