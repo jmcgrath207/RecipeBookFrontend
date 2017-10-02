c82cc131d4e137325d817a5e75a6a9855c6ef76a
sonance207 <sonance207@gmail.com>
2017-10-02 18:38:15 -0500

    added Auth Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -9,13 +8,12 @@ import {ShoppingListService} from './services/shoppingList.service';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeService} from './services/recipe.service';
 import {DataStorageService} from './Shared/data-storage.service';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import {AuthService} from './auth/auth.service';
 import {AuthGuardService} from './auth/auth-guard.service';
 import {RecipeModule} from './recipes/recipe.module';
 import {SharedModule} from './Shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
+import {AuthModule} from './auth/auth.module';
 
 
 
@@ -28,8 +26,6 @@ import {ShoppingListModule} from './shopping-list/shopping-list.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignupComponent,
-    SigninComponent
   ],
   imports: [
     BrowserModule,
@@ -37,7 +33,8 @@ import {ShoppingListModule} from './shopping-list/shopping-list.module';
     AppRoutingModule,
     RecipeModule,
     SharedModule,
-    ShoppingListModule
+    ShoppingListModule,
+    AuthModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuardService],
   bootstrap: [AppComponent]
