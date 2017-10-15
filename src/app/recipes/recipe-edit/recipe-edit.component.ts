cccc4c1e588f42035559d44d70bf8c5944ca6c50
John McGrath <8764013+jmcgrath207@users.noreply.github.com>
2017-10-14 20:46:50 -0500

    Update recipe-edit.component.ts
    Adding Get Controls

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -94,5 +94,9 @@ export class RecipeEditComponent implements OnInit {
       'ingredients' : recipeIngredients
     });
   }
+  
+    getControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
 
 }
